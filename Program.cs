f3c850f7909426cecbb4ffca093bbb6f2c6fede2
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-10-10 00:30:03 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using simplex_method;
 
 namespace simplex_method
 {
@@ -17,7 +18,7 @@ namespace simplex_method
 
         public SMX_MD(double[] c, double[,] A, double[] b)
         {
-            if(A.GetLength(0) != b.Length || A.GetLength(1) != c.Length)
+            if (A.GetLength(0) != b.Length || A.GetLength(1) != c.Length)
             {
                 throw new Exception("Неверное соотношение размеров матриц!");
             }
@@ -29,14 +30,14 @@ namespace simplex_method
 
             FreeX = new string[c.Length];
             int k = 0; // заполнение свободных иксов
-            for(int i = 0; i < c.Length; i++)
+            for (int i = 0; i < c.Length; i++)
             {
-                FreeX[i] = $"X{i+1}";
+                FreeX[i] = $"X{i + 1}";
                 k++;
             }
 
             DependX = new string[b.Length]; // заполнение зависимых иксов
-            for(int i = 0;i < b.Length; i++)
+            for (int i = 0; i < b.Length; i++)
             {
                 k++;
                 DependX[i] = $"X{k}";
@@ -45,7 +46,7 @@ namespace simplex_method
 
         public bool FindOptSolve()
         {
-            if(FindOprSolve())
+            if (FindOprSolve())
             {
                 int l = 0; // счетчик отрицательных элементов в строке F
                 for (int t = 1; t < c.Length + 1; t++) if (table[t, b.Length] < 0) { l++; }
@@ -53,7 +54,7 @@ namespace simplex_method
 
                 for (int i = 1; i < c.Length + 1; i++)
                 {
-                    if(table[i, b.Length] > 0)
+                    if (table[i, b.Length] > 0)
                     {
                         int razr_stolb = i;
 
@@ -61,8 +62,8 @@ namespace simplex_method
 
                         int flag2 = 0;
                         int razr_str = -1;
-                            
-                        for(int z = 0; z < b.Length; z++)
+
+                        for (int z = 0; z < b.Length; z++)
                         {
                             double k = table[0, z] / table[razr_stolb, z];
                             if (k < min && k > 0)
@@ -73,7 +74,7 @@ namespace simplex_method
                             }
                         }
 
-                        if(flag2 == 0) { continue; }
+                        if (flag2 == 0) { continue; }
                         fix_table(razr_str, razr_stolb);
                         return FindOptSolve();
                     }
@@ -88,9 +89,9 @@ namespace simplex_method
             double[,] table1 = new double[c.Length + 1, b.Length + 1];
 
             for (int i = 0; i < b.Length; i++) { table1[0, i] = b[i]; } // заполнение первой колонки начальной таблицы
-            table1[0,b.Length] = 0;
+            table1[0, b.Length] = 0;
 
-            for (int i = 0; i < b.Length; i++) 
+            for (int i = 0; i < b.Length; i++)
             {
                 for (int j = 1; j < c.Length + 1; j++)
                 {
@@ -98,7 +99,7 @@ namespace simplex_method
                 }
             }
 
-            for (int i = 1; i < c.Length + 1; i++) { table1[i,b.Length] = -c[i - 1]; } // на этом моменте заполнили всю начальную таблицу
+            for (int i = 1; i < c.Length + 1; i++) { table1[i, b.Length] = -c[i - 1]; } // на этом моменте заполнили всю начальную таблицу
             table = table1;
         }
 
@@ -122,8 +123,8 @@ namespace simplex_method
 
                             int flag1 = 0;
                             int razr_str = -1;
-                            
-                            for(int z = 0; z < b.Length; z++)
+
+                            for (int z = 0; z < b.Length; z++)
                             {
                                 double k = table[0, z] / table[razr_stolb, z];
                                 if (k < min && k > 0)
@@ -134,7 +135,7 @@ namespace simplex_method
                                 }
                             }
 
-                            if(flag1 == 0) { continue; }
+                            if (flag1 == 0) { continue; }
 
                             fix_table(razr_str, razr_stolb);
 
@@ -153,14 +154,14 @@ namespace simplex_method
 
             double r_e = table[razr_stolb, razr_str]; // разрешающий элемент
 
-            string _x = FreeX[razr_stolb-1];
-            FreeX[razr_stolb-1] = DependX[razr_str];
+            string _x = FreeX[razr_stolb - 1];
+            FreeX[razr_stolb - 1] = DependX[razr_str];
             DependX[razr_str] = _x;
 
-            table1[razr_stolb, razr_str] = 1/r_e;
+            table1[razr_stolb, razr_str] = 1 / r_e;
 
-            for(int i = 0; i < c.Length+1; i++) {
-                if(i != razr_stolb) table1[i, razr_str] = table[i, razr_str]/r_e;
+            for (int i = 0; i < c.Length + 1; i++) {
+                if (i != razr_stolb) table1[i, razr_str] = table[i, razr_str] / r_e;
             }
 
             for (int i = 0; i < b.Length + 1; i++)
@@ -168,11 +169,11 @@ namespace simplex_method
                 if (i != razr_str) table1[razr_stolb, i] = -table[razr_stolb, i] / r_e;
             }
 
-            for(int i = 0;i < b.Length+1; i++)
+            for (int i = 0; i < b.Length + 1; i++)
             {
-                for(int j = 0;  j < c.Length + 1; j++)
+                for (int j = 0; j < c.Length + 1; j++)
                 {
-                    if (i != razr_str && j != razr_stolb) table1[j, i] = table[j, i] - (table[razr_stolb,i] * table[j, razr_str]) / r_e;
+                    if (i != razr_str && j != razr_stolb) table1[j, i] = table[j, i] - (table[razr_stolb, i] * table[j, razr_str]) / r_e;
                 }
             }
 
@@ -181,27 +182,27 @@ namespace simplex_method
 
         public void Print() // выводит таблицу
         {
-            for(int i = 0; i < b.Length+1; i++)
+            for (int i = 0; i < b.Length + 1; i++)
             {
                 if (i != b.Length) Console.Write(DependX[i] + "\t"); else Console.Write("F" + "\t");
-                
-                for(int j = 0;j < c.Length + 1;j++) Console.Write(Math.Round(table[j, i],2) + "\t"); 
+
+                for (int j = 0; j < c.Length + 1; j++) Console.Write(Math.Round(table[j, i], 2) + "\t");
                 Console.WriteLine();
             }
             Console.WriteLine();
             Console.Write("\t");
             Console.Write("S" + "\t");
             foreach (object obj in FreeX) { Console.Write(obj + "\t"); }
-        }   
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            SMX_MD a = new SMX_MD(new double[] { -7,-8,-3 }, new double[,] { { 3,1,1 }, { 1,4,0.5 }, { 1,0,2} }, new double[] { 4,7,8}); // условие задачи принимаются в каноническом виде
-            a.FindOptSolve();
-            a.Print();
-            Console.WriteLine();
         }
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        SMX_MD a = new SMX_MD(new double[] { -7,-8,-3 }, new double[,] { { 3,1,1 }, { 1,4,0.5 }, { 1,0,2} }, new double[] { 4,7,8}); // условие задачи принимаются в каноническом виде
+        a.FindOptSolve();
+        a.Print();
+        Console.WriteLine();
     }
 }
